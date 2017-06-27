e25731dc32bca307eda639cb42bb5451ff7de0b6
Michel Aquino <michel.has@gmail.com>
2017-06-26 21:57:18 -0300

    Add request log data middleware

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/labstack/echo"
 	"github.com/michelaquino/golang_kafka_example/context"
 	"github.com/michelaquino/golang_kafka_example/handlers"
+	apiMiddleware "github.com/michelaquino/golang_kafka_example/middleware"
 )
 
 func main() {
 	logger := context.GetLogger()
 
 	echoInstance := echo.New()
+
+	// Middlewares
+	echoInstance.Use(apiMiddleware.RequestLogDataMiddleware())
 
 	echoInstance.GET("/healthcheck", handlers.Healthcheck)
 
